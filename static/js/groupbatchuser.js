706c5210b0f3f8c3bf7f839c2058796eb89f56b6
Phy <git@phy25.com>
2020-02-26 14:47:49 -0500

    Add import meta axios request

--- static/js/groupbatchuser.js
+++ static/js/groupbatchuser.js
@@ -1,9 +1,24 @@
 /* phy@bitnp.net */
 if(window.Vue){
+    var axiosGlobalCatch = function(error){
+        if (error.response && error.response.status == 401) {
+            alert('请刷新页面重新登录。');
+            return;
+        }
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            alert('请求失败 ('+error.response.status+'): '+JSON.stringify(error.response.data));
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            alert('请求失败: '+error.message);
+        }
+    };
     Vue.component('comp-importer', {
         props: {userList: Array},
         data: function(){return {
             importByGroupPath: '',
+            importByGroupProcessing: 0,
             importByListText: sessionStorage['groupbatch_importByListText'] || '',
         }},
         watch: {
@@ -20,7 +35,24 @@ if(window.Vue){
         methods: {
             importByGroup: function (event) {
                 if(event) event.preventDefault();
-                alert(this.importByGroupPath);
+                var addItem = this.addItem, _this = this;
+                _this.importByGroupProcessing = 1;
+                axios({
+                    method: 'GET',
+                    url: './',
+                    params: {
+                        path: this.importByGroupPath
+                    }
+                })
+                    .then(function(response){
+                        response.data[0].members.forEach(function(user){
+                            addItem(user);
+                        });
+                    })
+                    .catch(axiosGlobalCatch)
+                    .finally(function(){
+                        _this.importByGroupProcessing = 0;
+                    });
             },
             importByList: function (event) {
                 if(event) event.preventDefault();
@@ -41,6 +73,8 @@ if(window.Vue){
                 if (item.opState == undefined){
                     item.opState = 0;
                 }
+                item.errorMessage = '';
+                item.metaLoading = false;
                 // TODO: duplicate elimination
                 this.userList.push(item);
             }
@@ -54,12 +88,56 @@ if(window.Vue){
                 this.userList.splice(index, 1);
             },
             loadItemMeta: function(index){
-                Vue.set(this.userList[index], 'errorMessage', 'DEV');
+                var userList = this.userList, thisitem = userList[index];
+                thisitem.metaLoading = true;
+                axios({
+                    method: 'GET',
+                    url: '../users/',
+                    params: {
+                        search: thisitem.username
+                    }
+                })
+                    .then(function(response){
+                        var cur_user = response.data[0];
+                        if (response.data.length > 1){
+                            // opps, this is admin
+                            var fi = response.data.filter(function(user){
+                                return user.username == thisitem.username || user.email == thisitem.email;
+                            });
+                            cur_user = fi[0];
+                        }
+                        if (!cur_user){
+                            thisitem.errorMessage = '找不到符合条件的用户';
+                            return;
+                        }
+                        Vue.set(userList, index, Object.assign({}, userList[index], cur_user));
+                    })
+                    .catch(function(error){
+                        if (error.response && error.response.status == 404) {
+                            thisitem.errorMessage = '找不到符合条件的用户';
+                            return;
+                        }
+                        axiosGlobalCatch(error);
+                    })
+                    .finally(function(){
+                        userList[index].metaLoading = false;
+                        // Vue.set(userList, index, Object.assign({}, , {metaLoading: false}));
+                    });
             }
         }
     });
     Vue.component('comp-list-item', {
-        props: {username: String, email: String, opState: Number, enabled: String, errorMessage: String, name: String, createdTimestamp: String, index: Number},
+        props: {
+            username: String,
+            email: String,
+            opState: Number,
+            enabled: String,
+            name: String,
+            createdTimestamp: String,
+            index: Number,
+            errorMessage: String,
+            metaLoading: Boolean
+        },
         template: '#comp-list-item-template',
         methods: {
             loadMeta: function(){
